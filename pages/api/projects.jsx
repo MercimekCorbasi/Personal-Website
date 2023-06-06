e1d529b1a6f6bc43fa49ce2c9549c28877df49df
MercimekCorbasi <88063872+MercimekCorbasi@users.noreply.github.com>
2023-06-06 18:35:34 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -2,15 +2,15 @@ export default async (req, res) => {
   const data = [
     {
       id: 1,
-      image: "https://cdn.discordapp.com/attachments/956625660347031663/1115654239511916624/B3645CE9-DBC9-410C-9A5C-2ABC4422FC78.jpg",
-      name: "Code Share",
-      description: "Benim işte",
+      image: "https://cdn.discordapp.com/attachments/956625660347031663/1115664228888608788/19397F38-F554-4852-A6D8-FCB814569C25.jpg",
+      name: "bad boy, pussy magnet",
+      description: "Buyrun benim",
       link: "https://www.instagram.com/alikartalm/",
     },
     {
       id: 2,
-      image: "https://media.discordapp.net/attachments/737348411568685066/999563078079361074/YT_AVATAR_NET.jpg",
-      name: "Youtube Channel",
+      image: "https://cdn.discordapp.com/attachments/956625660347031663/1115664229261914122/B3645CE9-DBC9-410C-9A5C-2ABC4422FC78.jpg",
+      name: "karizma focus, o baby",
       description: "Nam-ı değer mercimek",
       link: "https://www.instagram.com/alikartalm/",
     }
