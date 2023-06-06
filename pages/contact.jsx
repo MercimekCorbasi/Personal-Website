3b6bfaa0fb3ebe4303d610512836c727b3be65d1
MercimekCorbasi <88063872+MercimekCorbasi@users.noreply.github.com>
2023-06-06 18:57:24 +0300

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <>
       <div className="py-20">
-        <p className="text-3xl text-white font-semibold text-center">Contact Me</p>
-        <p className="text-xl text-white/50 font-normal text-center mb-5">Other addresses where you can contact me.</p>
+        <p className="text-3xl text-white font-semibold text-center">İletişim bilgilerim</p>
+        <p className="text-xl text-white/50 font-normal text-center mb-5">Spam, taciz ve trol yapmayın.</p>
         <div className=" flex justify-center flex-col items-center">
             <div className="max-w-lg w-full">
                 <a target="_blank" rel="norefferer" href=" https://instagram.com/alikartalm/">
