486066a0e8f649e01cf604c66cc9a300829cc267
MercimekCorbasi <88063872+MercimekCorbasi@users.noreply.github.com>
2023-06-06 17:52:21 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -2,7 +2,7 @@ export default async (req, res) => {
   const data = [
     {
       id: 1,
-      image: "https://media.discordapp.net/attachments/737348411568685066/954502826791428136/codeshare_logo.png",
+      image: "https://cdn.discordapp.com/attachments/956625660347031663/1115654239511916624/B3645CE9-DBC9-410C-9A5C-2ABC4422FC78.jpg",
       name: "Code Share",
       description: "Discord türkiyenin en büyük discord bot komutları arşivi ve en büyük discord botu uptime, hostlayıcı websitesi. Code Share veritabanında discord botlarına özgü yüzlerce discord bot komutu bulunmaktadır ve kendi discord botunuzu yaratmanıza yardımcı olur.",
       link: "https://codeshare.me",
