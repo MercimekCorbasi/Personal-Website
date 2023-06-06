a0db7ae1a3177559fc1db3568703a091a3ec96f0
MercimekCorbasi <88063872+MercimekCorbasi@users.noreply.github.com>
2023-06-06 18:03:20 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -5,14 +5,14 @@ export default async (req, res) => {
       image: "https://cdn.discordapp.com/attachments/956625660347031663/1115654239511916624/B3645CE9-DBC9-410C-9A5C-2ABC4422FC78.jpg",
       name: "Code Share",
       description: "Benim işte",
-      link: "https://codeshare.me",
+      link: "https://www.instagram.com/alikartalm/",
     },
     {
       id: 2,
       image: "https://media.discordapp.net/attachments/737348411568685066/999563078079361074/YT_AVATAR_NET.jpg",
       name: "Youtube Channel",
-      description: "I am a professional editor and intermediate software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management. I post videos about discord on my youtube channel.",
-      link: "https://youtube.com/UmutBayraktarYT",
+      description: "Nam-ı değer mercimek",
+      link: "https://www.instagram.com/alikartalm/",
     }
   ];
   res.status(200).json(data);
