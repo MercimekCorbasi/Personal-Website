5d034b7a806fc2dba8e44bd1a7671e0e03df6818
MercimekCorbasi <88063872+MercimekCorbasi@users.noreply.github.com>
2023-06-06 18:54:33 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,7 +26,7 @@ export default function Home() {
             
                 </div>
                 <p className="text-white/50 text-md mt-3">
-Biz buralara yeni gelmedik geri geldik ha babba
+sanal magandayım , ifşalanmam , yıkılmam , ezilmem , bükülmem , sarsılmam , kayıtlara geçmiş 57 tane hakaret , 29 tane adam yralama , 9 tane ciayet davam var , ileri derecede bipolarım istediğim kişiyi kolay bir şekilde in***ara sürüklerim, kimse baş edemez , sanalın tek mafyası benim , sevdiğimi korurum , laf atanı ifşalarım , ailesine haciz getirtirim , baba parası yemem kendi paramı alın teriyle kazanır parayı karımla çatur çutur yerim, sicilim çok kabarık , 3 yıldız polisler tarafından aranıyorum , aşırı derecede karizmayım , insan anlamam , birinci vazifem kadınlardır , ne mutlu türkiyem sanal maganda
                 </p>
               </div>
               <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
